60c02bf4e940f0547582fdd5e5a9d9725f19b241
Shatha Alrayyani <shatha.alrayyani223@gmail.com>
2022-05-09 05:41:19 +0300

    sec challenge solved

--- code-challenges/challenges-09.js
+++ code-challenges/challenges-09.js
@@ -20,15 +20,11 @@
 
 const arrInc = (arr) => {
     let result = [];
-    
-    arr.forEach(add)
-    
-    function add(){
-        result = arr [i] + 10 ;
-    
-    }
-    // write your code here
-    return result;
+   arr.forEach(item => {
+       result.push(item + 10);
+       
+   })
+   return result;
 }
 // -------------------------------------------------------------------------------------------------------
 
